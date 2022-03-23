b94452a99e1f5201bba4f93e708a2f64e3f66bbf
Maciej Malecki <maciej.malecki@capgemini.com>
2022-03-23 15:52:04 +0100

    DOM primers

--- src/app/book/components/book-list/book-list.component.spec.ts
+++ src/app/book/components/book-list/book-list.component.spec.ts
@@ -2,49 +2,84 @@ import { BookListComponent } from './book-list.component';
 import {Book} from "../../model/book";
 import createSpy = jasmine.createSpy;
 import {BehaviorSubject} from "rxjs";
+import {ComponentFixture, TestBed} from "@angular/core/testing";
+import {BookService} from "../../service/book.service";
 
 describe('BookListComponent', () => {
 
-  let component: BookListComponent;
-  let bookService: any;
-  let spinnerService: any
-  let aBook: Book;
+  describe("[class]", () => {
 
-  beforeEach(() => {
-    bookService = {
-      books$: new BehaviorSubject<Book[]>([]),
-      updateBook: createSpy()
-    };
-    spinnerService = {
-      show: createSpy()
-    };
-    component = new BookListComponent(bookService, spinnerService);
-    aBook = {
-      id: 0,
-      author: 'foo',
-      title: 'bar',
-      year: 2022
-    };
+    let component: BookListComponent;
+    let bookService: any;
+    let spinnerService: any
+    let aBook: Book;
+
+    beforeEach(() => {
+      bookService = {
+        books$: new BehaviorSubject<Book[]>([]),
+        updateBook: createSpy()
+      };
+      spinnerService = {
+        show: createSpy()
+      };
+      component = new BookListComponent(bookService, spinnerService);
+      aBook = {
+        id: 0,
+        author: 'foo',
+        title: 'bar',
+        year: 2022
+      };
+    });
+
+    it("there is no selected book in a fresh new BookList component", () => {
+      expect(component.selectedBook).toBeFalsy();
+    });
+
+    it("once we select a book, it becomes selected", () => {
+      // when
+      component.selectBook(aBook);
+      // then
+      expect(component.selectedBook).toBeTruthy();
+      expect(component.selectedBook).toEqual(aBook);
+    });
+
+    it("saving book that is not selected is kind of weird", () => {
+      // when
+      component.saveBook(aBook);
+      // then
+      expect(component.selectedBook).toBeFalsy();
+      expect(spinnerService.show).toHaveBeenCalledTimes(2);
+      expect(bookService.updateBook).toHaveBeenCalledWith(aBook);
+    });
   });
 
-  it("there is no selected book in a fresh new BookList component", () => {
-    expect(component.selectedBook).toBeFalsy();
-  });
+  describe("[DOM]", () => {
 
-  it("once we select a book, it becomes selected", () => {
-    // when
-    component.selectBook(aBook);
-    // then
-    expect(component.selectedBook).toBeTruthy();
-    expect(component.selectedBook).toEqual(aBook);
-  });
+    let component: BookListComponent;
+    let fixture: ComponentFixture<BookListComponent>;
+    let nativeElement: HTMLElement;
 
-  it("saving book that is not selected is kind of weird", () => {
-    // when
-    component.saveBook(aBook);
-    // then
-    expect(component.selectedBook).toBeFalsy();
-    expect(spinnerService.show).toHaveBeenCalledTimes(2);
-    expect(bookService.updateBook).toHaveBeenCalledWith(aBook);
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        declarations: [BookListComponent],
+        providers: [BookService]
+      }).compileComponents();
+    });
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(BookListComponent);
+      component = fixture.componentInstance;
+      nativeElement = fixture.nativeElement;
+    });
+
+    it("component can be created", () => {
+      expect(component).toBeTruthy();
+    });
+
+    it("has a nice title", () => {
+      const titleElement = nativeElement.querySelector(".book-list--title");
+      expect(titleElement?.textContent).toBe("Book list");
+    });
+
   });
 });
