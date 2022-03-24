3e8120b745c3330563fe59bc4008ece6683e753f
Maciej Malecki <maciej.malecki@capgemini.com>
2022-03-24 10:33:21 +0100

    when stable

--- src/app/book/components/book-details/book-details.component.spec.ts
+++ src/app/book/components/book-details/book-details.component.spec.ts
@@ -1,17 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookDetailsComponent } from './book-details.component';
+import {Book} from "../../model/book";
+import {FormsModule} from "@angular/forms";
 
 describe('BookDetailsComponent', () => {
 
-  fdescribe("[DOM]", () => {
+  describe("[DOM]", () => {
     let component: BookDetailsComponent;
     let fixture: ComponentFixture<BookDetailsComponent>;
     let nativeElement: HTMLElement;
+    let aBook: Book;
+    // DOM until functions
+    const getBookEditor = () => nativeElement.querySelector('#book-editor') as HTMLElement;
+    const getNoBookPanel = () => nativeElement.querySelector('#please-select-a-book') as HTMLElement;
+    const getInputField = (id: string) => nativeElement.querySelector(`input#${id}`) as HTMLInputElement;
 
     beforeEach(async () => {
       await TestBed.configureTestingModule({
-        declarations: [BookDetailsComponent]
+        declarations: [BookDetailsComponent],
+        imports: [FormsModule]
       })
         .compileComponents();
     });
@@ -21,6 +29,13 @@ describe('BookDetailsComponent', () => {
       component = fixture.componentInstance;
       nativeElement = fixture.nativeElement as HTMLElement;
       fixture.detectChanges();
+
+      aBook = {
+        id: 4,
+        author: 'Douglas Crockford',
+        title: 'JavaScript: The Good Parts',
+        year: 2022
+      };
     });
 
     it('should create', () => {
@@ -28,11 +43,27 @@ describe('BookDetailsComponent', () => {
     });
 
     it('initially no book is selected so appropriate message is shown', () => {
-      const sectionElement = nativeElement.querySelector('#please-select-a-book');
-      expect(sectionElement).toBeTruthy();
-      expect(sectionElement!.textContent).toContain('Please select a book.');
-      const editorElement = nativeElement.querySelector('#book-editor');
-      expect(editorElement).toBeFalsy();
+      const noBookPanel = getNoBookPanel();
+      expect(noBookPanel).toBeTruthy();
+      expect(noBookPanel!.textContent).toContain('Please select a book.');
+
+      expect(getBookEditor()).toBeFalsy();
+    });
+
+    it('once book is given via binding, it is displayed', async () => {
+      // given
+      component.book = aBook;
+      // when
+      fixture.detectChanges();
+      // then
+      const bookEditor = getBookEditor();
+      expect(bookEditor).toBeTruthy();
+
+      await fixture.whenStable();
+
+      expect(getInputField('title')!.value).toBe(aBook.title);
+      expect(getInputField('author')!.value).toBe(aBook.author);
+      expect(getInputField('year')!.value).toBe(`${aBook.year}`);
     });
   });
 });
